d4e20823f674bc8cbd93cdf7b3e2e6b733dec7e7
mrreddy12 <126330843+mrreddy12@users.noreply.github.com>
2024-04-08 12:34:43 +0530

    Update Main.javaff

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-          // added in master
+          // added in masterss
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
